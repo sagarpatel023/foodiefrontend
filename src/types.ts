1b0d23141083d8fe39276e40bab36543d85a3bcd
Sagar Patel <sagarp2002@hotmail.com>
2024-07-04 17:31:38 +1200

    search page and filters

--- src/types.ts
+++ src/types.ts
@@ -26,3 +26,12 @@ export type MenuItem = {
     imageUrl: string;
     lastUpdated: string;
   };
+
+  export type RestaurantSearchResponse = {
+    data: Restaurant[];
+    pagination: {
+      total: number;
+      page: number;
+      pages: number;
+    };
+  };
